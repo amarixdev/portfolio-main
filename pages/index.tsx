8f836d4671afbb56f7a90142668b842c8f5e9136
amarixdev <amaridev@alumni.unc.edu>
2023-06-24 00:10:22 -0400

    bug fix: parallax scroll flicker

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,22 @@
-
+import { NextPage } from "next";
 import Image from "next/image";
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { BsTwitter } from "react-icons/bs";
+import { FaRedditAlien } from "react-icons/fa";
+import { HiMusicNote } from "react-icons/hi";
+import RedditTheme from "../components/RedditTheme";
 import ParallaxBg from "../public/assets/profile-bg.png";
 import ParallaxFg from "../public/assets/profile-padding.png";
 import style from "../styles/style.module.css";
-import { BsFillPersonFill, BsTwitter } from "react-icons/bs";
-import { GoMail } from "react-icons/go";
-import Avatar from "../public/assets/reddit-avatar.png";
-import { title } from "process";
-import { HiMusicNote } from "react-icons/hi";
-import { FaRedditAlien } from "react-icons/fa";
-import { useMediaQuery } from "../util/hooks";
 import { capitalizeString } from "../util/functions";
-import { NextPage } from "next";
-import RedditTheme from "../components/RedditTheme";
+import { useMediaQuery } from "../util/hooks";
 
 type Props = {};
 
 const Home: NextPage = (props: Props) => {
   const titleRef = useRef<HTMLDivElement>(null);
   const wrapperRef = useRef<HTMLDivElement>(null);
+  const themeRef = useRef<HTMLDivElement>(null);
   const isBreakPoint = useMediaQuery(1023);
   const iconSize = isBreakPoint ? 45 : 65;
   const [displayTitle, setDisplayTitle] = useState(false);
@@ -63,8 +60,9 @@ const Home: NextPage = (props: Props) => {
         const elementRect = titleRef.current.getBoundingClientRect();
         const isPastElement = isBreakPoint
           ? elementRect.top <= 630
-          : elementRect.top <= 700;
+          : elementRect.top <= 680;
         setDisplayTitle(isPastElement);
+        console.log(elementRect.top);
       }
     };
 
@@ -78,94 +76,109 @@ const Home: NextPage = (props: Props) => {
   const [theme, setTheme] = useState("");
 
   return (
-    <div ref={wrapperRef} className={`${style.wrapper}`}>
-      <div className={`${style.header}`}>
-        <Image
-          priority
-          src={ParallaxBg}
-          alt="bg"
-          className={`${style.background} ${heroLoaded ? "block" : "hidden"} `}
-          onLoad={() => setHeroLoaded(true)}
-        />
-        <Image
-          src={ParallaxFg}
-          alt="fg"
-          priority
-          className={`${style.foreground} ${
-            heroLoaded ? "block" : "hidden"
-          } h-full w-full lg:h-fit`}
-          quality={100}
-        />
-        <div
-          className={`${style.display} bg-black w-full flex flex-col justify-top pt-10 items-center `}
-        >
+    <div>
+      {/* <div className="w-full bg-black h-20 fixed z-[999] "></div> */}
+      <div ref={wrapperRef} className={`${style.wrapper}`}>
+        <section>
+          <Image
+            priority
+            src={ParallaxBg}
+            alt="bg"
+            className={`${style.background} ${
+              heroLoaded ? "block" : "hidden"
+            } h-full lg:h-fit `}
+            onLoad={() => setHeroLoaded(true)}
+          />
           <div
-            ref={titleRef}
-            className={`w-full items-center justify-center flex flex-col gap-3 ${
-              displayTitle ? "opacity-100" : "opacity-0"
-            } transition duration-500 ease-in-out`}
+            className={` absolute h-full lg:h-fit w-full xl:top-[-5%] 2xl:top-[-20%] ${
+              style.test
+            } ${heroLoaded ? "block" : "hidden"}  `}
           >
-            <p className=" font-medium  text-3xl lg:text-4xl">
-              {" "}
-              Amari <span className="text-[#87d3fe]">DeV</span>aughn
-            </p>
-            <p className="font-bold text-5xl lg:text-6xl text-center text-[#87d3fe] ">
-              Software <span className="text-white">DeV</span>eloper
-            </p>
+            <Image
+              src={ParallaxFg}
+              alt="fg"
+              priority
+              quality={100}
+              className={`relative object-cover h-full w-full`}
+            />
 
             <div
-              className={`${
-                displayTitle ? "opacity-100" : "opacity-0"
-              } w-full mt-10 lg:mt-14 flex flex-col items-center justify-center`}
+              className={`w-full relative bottom-20 pt-10 lg:pb-[160px] bg-black flex flex-col justify-top items-center `}
             >
-              <h1 className="font-bold text-3xl min-w-[230px] ">
-                {theme ? "Theme: " : "Choose Portfolio Theme"}
-                <span className="font-extrabold">
-                  {capitalizeString(theme)}
-                </span>
-              </h1>
-              <div className="flex mt-10 lg:mt-16 justify-evenly w-full">
-                <button
-                  id="music"
-                  className={`${style.musicIcon} w-20 h-20 lg:w-24 lg:h-24 rounded-2xl flex items-center justify-center cursor-pointer`}
-                  onClick={() => {
-                    handleThemeSelection("music");
-                  }}
-                >
-                  <HiMusicNote size={iconSize} />
-                </button>
-                <button
-                  id="reddit"
-                  className={`${style.redditIcon}  w-20 h-20 lg:w-24 lg:h-24 flex items-center justify-center rounded-2xl p-2 cursor-pointer`}
-                  onClick={() => handleThemeSelection("reddit")}
-                >
-                  <FaRedditAlien size={iconSize} />
-                </button>
+              <div
+                ref={titleRef}
+                className={`w-full items-center justify-center flex flex-col gap-3 ${
+                  displayTitle ? "opacity-100" : "opacity-0"
+                } transition duration-500 ease-in-out`}
+              >
+                <p className=" font-medium  text-3xl lg:text-4xl">
+                  {" "}
+                  Amari <span className="text-[#87d3fe]">DeV</span>aughn
+                </p>
+                <p className="font-bold text-5xl lg:text-6xl text-center text-[#87d3fe] ">
+                  Software <span className="text-white">DeV</span>eloper
+                </p>
 
-                <button
-                  id="twitter"
-                  className={`${style.twitterIcon} w-20 h-20 lg:w-24 lg:h-24 flex items-center justify-center rounded-2xl p-2 cursor-pointer`}
-                  onClick={() => handleThemeSelection("twitter")}
+                <div
+                  className={`${
+                    displayTitle ? "opacity-100" : "opacity-0"
+                  } w-full mt-10 lg:mt-14 flex flex-col items-center justify-center`}
                 >
-                  <BsTwitter size={iconSize} />
-                </button>
-              </div>
-              {
-                <div className="mt-14 w-full flex justify-center">
-                  <h1
-                    className="font-medium text-xl"
-                    onClick={() => handleThemeSelection("none")}
-                  >
-                    No Theme
+                  <h1 className="font-bold text-3xl min-w-[230px] ">
+                    {theme ? "Theme: " : "Choose Portfolio Theme"}
+                    <span className="font-extrabold">
+                      {capitalizeString(theme)}
+                    </span>
                   </h1>
+                  <div className="flex mt-10 lg:mt-16 justify-evenly w-full">
+                    <button
+                      id="music"
+                      className={`${style.musicIcon} w-20 h-20 lg:w-24 lg:h-24 rounded-2xl flex items-center justify-center cursor-pointer`}
+                      onClick={() => {
+                        handleThemeSelection("music");
+                      }}
+                    >
+                      <HiMusicNote size={iconSize} />
+                    </button>
+                    <button
+                      id="reddit"
+                      className={`${style.redditIcon}  w-20 h-20 lg:w-24 lg:h-24 flex items-center justify-center rounded-2xl p-2 cursor-pointer`}
+                      onClick={() => {
+                        handleThemeSelection("reddit");
+                      }}
+                    >
+                      <FaRedditAlien size={iconSize} />
+                    </button>
+
+                    <button
+                      id="twitter"
+                      className={`${style.twitterIcon} w-20 h-20 lg:w-24 lg:h-24 flex items-center justify-center rounded-2xl p-2 cursor-pointer`}
+                      onClick={() => handleThemeSelection("twitter")}
+                    >
+                      <BsTwitter size={iconSize} />
+                    </button>
+                  </div>
+                  {
+                    <div className="mt-14 w-full flex justify-center">
+                      <h1
+                        className="font-medium text-xl"
+                        onClick={() => handleThemeSelection("none")}
+                      >
+                        No Theme
+                      </h1>
+                    </div>
+                  }
                 </div>
-              }
+              </div>
+            </div>
+            <div
+              ref={themeRef}
+              className="w-full absolute bg-black lg:bottom-[-820px]"
+            >
+              {theme === "reddit" && <RedditTheme />}
             </div>
           </div>
-          <div className="w-full h-screen pt-10 bg-black">
-            {theme === "reddit" && <RedditTheme />}
-          </div>
-        </div>
+        </section>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
 
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      base: "1px",
+      xs: "390px",
+      // => @media (min-width: 390px) { ... }
+
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+
+      "3xl": "1800px",
+      // => @media (min-width: 1800px) { ... }
+
+      "4xl": "2200px",
+      // => @media (min-width: 2200px) { ... }
+    },
   },
-  plugins: [],
 };
